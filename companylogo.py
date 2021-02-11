1ac36841186ae734e66b3654a61faf935f4072cd
Farnoosh Fathaliani <ffathaliani@Farnooshs-MacBook-Pro.local>
2021-02-11 13:42:59 -0500

    added solutions

--- companylogo.py
+++ companylogo.py
@@ -1,1 +1,9 @@
+import collections
 
+if __name__ == '__main__':
+    s = input()
+    # y = collections.Counter(sorted(s))
+    # print(y)
+    # x = collections.Counter(sorted(s)).most_common(3)
+    # print(x)
+    [print(*c) for c in collections.Counter(sorted(s)).most_common(3)]
